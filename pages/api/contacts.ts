d4960b2fd40bd9f72e3bf435943ffda15003cd05
Om  Patil <omsandeeppatil02@gmail.com>
2025-03-20 06:01:44 +0000

    BaseCode

--- pages/api/contacts.ts
+++ pages/api/contacts.ts
@@ -2,10 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 let contacts = [
   { id: 1, name: "Durva D", amount: "120", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-5.png?raw=true" },
-  { id: 2, name: "Arjun R", amount: "50", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-1.png?raw=true" },
-  { id: 3, name: "Aditya N", amount: "200", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-2.png?raw=true" },
+  { id: 2, name: "Aviraj P", amount: "50", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-1.png?raw=true" },
+  { id: 3, name: "Dhanyakumar M", amount: "200", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-2.png?raw=true" },
   { id: 4, name: "Shivam M", amount: "75", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-3.png?raw=true" },
-  { id: 5, name: "Yash K", amount: "150", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-4.png?raw=true" },
+  { id: 5, name: "Arjun R", amount: "150", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-4.png?raw=true" },
+  { id: 6, name: "Gargie D", amount: "50", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-1.png?raw=true" },
+  { id: 7, name: "Sohan B", amount: "200", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-2.png?raw=true" },
+  { id: 8, name: "Harsh P", amount: "75", image: "https://github.com/omsandippatil/Hawala/blob/main/img/avatar-3.png?raw=true" },
 ];
 
 
